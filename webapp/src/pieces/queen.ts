a7c0ad916f6a8a87165eca88dd6a509b2a355d81
Jonas <Jonas.ellington@gmail.com>
2022-11-03 12:43:03 -0500

    Queen movement

--- webapp/src/pieces/queen.ts
+++ webapp/src/pieces/queen.ts
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import isOccupied from "../helpers/squareOccupied";
 
 export default class Queen extends Piece {
     constructor(player: number) {
@@ -8,6 +9,96 @@ export default class Queen extends Piece {
     }
 
     listValidMoves(squares: Piece[][], i: number, j: number): number[][] {
-        return [];
+
+        let moves = [];
+        let iOffset: number;
+        let jOffset: number;
+
+        // going up
+        iOffset = 1;
+        jOffset = 0;
+        while (i + iOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset += 1;
+        }
+        if (i + iOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going down
+        iOffset = -1;
+        jOffset = 0;
+        while (i + iOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset -= 1;
+        }
+        if (i + iOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going right
+        iOffset = 0;
+        jOffset = 1;
+        while (j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            jOffset += 1;
+        }
+        if (j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going down
+        iOffset = 0;
+        jOffset = -1;
+        while (j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            jOffset -= 1;
+        }
+        if (j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+
+        // going up/right
+        iOffset = 1;
+        jOffset = 1;
+        while (i + iOffset < 8 && j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset += 1;
+            jOffset += 1;
+        }
+        if (i + iOffset < 8 && j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going up/left
+        iOffset = 1;
+        jOffset = -1;
+        while (i + iOffset < 8 && j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset += 1;
+            jOffset -= 1;
+        }
+        if (i + iOffset < 8 && j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going down/right
+        iOffset = -1;
+        jOffset = 1;
+        while (i + iOffset > -1 && j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset -= 1;
+            jOffset += 1;
+        }
+        if (i + iOffset > -1 && j + jOffset < 8 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        // going down/left
+        iOffset = -1;
+        jOffset = -1;
+        while (i + iOffset > -1 && j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === 0) {
+            moves.push([i + iOffset, j + jOffset]);
+            iOffset -= 1;
+            jOffset -= 1;
+        }
+        if (i + iOffset > -1 && j + jOffset > -1 && isOccupied(squares, i + iOffset, j + jOffset) === (this.player===1?2:1)) {
+            moves.push([i + iOffset, j + jOffset]);
+        }
+        return moves
     }
 }
